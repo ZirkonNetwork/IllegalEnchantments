0bec907eb6fef6ef4b45b5c79a65e1e66211f5de
Elephant1214 <nathangetsmail@gmail.com>
2022-09-22 20:24:35 -0600

    V2 somewhat works, not sure what's throwing a null pointer or how

--- src/main/java/nathan/illegalenchantments/IllegalEnchantments.java
+++ src/main/java/nathan/illegalenchantments/IllegalEnchantments.java
@@ -1,11 +1,11 @@
 package nathan.illegalenchantments;
 
-import nathan.illegalenchantments.event.PrepareAnvilEvents;
+import nathan.illegalenchantments.event.PrepareAnvilEventsV2;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class IllegalEnchantments extends JavaPlugin {
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.1";
     public static int maximumLevel;
     public static JavaPlugin plugin = null;
 
@@ -15,6 +15,6 @@ public final class IllegalEnchantments extends JavaPlugin {
         saveDefaultConfig();
         maximumLevel = getConfig().getInt("maximum-level");
 
-        getServer().getPluginManager().registerEvents(new PrepareAnvilEvents(), this);
+        getServer().getPluginManager().registerEvents(new PrepareAnvilEventsV2(), this);
     }
 }
---
+++ src/main/java/nathan/illegalenchantments/event/PrepareAnvilEventsV2.java
@@ -0,0 +1,201 @@
+package nathan.illegalenchantments.event;
+
+import nathan.illegalenchantments.IllegalEnchantments;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@SuppressWarnings("ConstantConditions")
+public class PrepareAnvilEventsV2 implements Listener {
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void processIllegalEnchantments(PrepareAnvilEvent event) {
+        final AnvilInventory anvilInventory = event.getInventory();
+        anvilInventory.setMaximumRepairCost(IllegalEnchantments.maximumLevel);
+
+        if (anvilInventory.getItem(0) != null && anvilInventory.getItem(1) != null && event.getResult() != null) {
+            final ItemStack input = anvilInventory.getItem(0);
+            final ItemStack input1 = anvilInventory.getItem(1);
+            ItemStack result = event.getResult();
+
+            if (input.getItemMeta() != null && input1.getItemMeta() != null && result.getItemMeta() != null) {
+                if (input.getItemMeta().hasEnchants() && input1.getItemMeta().hasEnchants() && input.getType() == input1.getType()) {
+                    final Map<Enchantment, Integer> resultEnchantments = result.getEnchantments();
+                    Map<Enchantment, Integer> finalEnchantments = new HashMap<>();
+
+                    for (Enchantment enchantment : enchantmentSet(input)) {
+                        if (enchantment.getMaxLevel() > 1) {
+                            if (levelsCanBeAdded(enchantment, input, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, input.getEnchantmentLevel(enchantment) + 1);
+                            } else if (firstInputIsHigher(enchantment, input, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, input.getEnchantmentLevel(enchantment));
+                            } else if (enchNotMoreThanDouble(enchantment, input, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, input.getEnchantmentLevel(enchantment));
+                            }
+                        }
+                    }
+
+                    for (Enchantment enchantment : enchantmentSet(input1)) {
+                        if (enchantment.getMaxLevel() > 1) {
+                            if (firstInputIsHigher(enchantment, input1, input, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, input1.getEnchantmentLevel(enchantment));
+                            } else if (enchNotMoreThanDouble(enchantment, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, input1.getEnchantmentLevel(enchantment));
+                            }
+                        }
+                    }
+
+                    // remove conflicting and add result enchantments then save result
+                    event.setResult(createResult(input, input1, result, finalEnchantments));
+                } else if (input.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta && input.getType() == input1.getType()) {
+                    final EnchantmentStorageMeta enchantmentStorageMeta1 = (EnchantmentStorageMeta) input1.getItemMeta();
+                    final Map<Enchantment, Integer> resultEnchantments = result.getItemMeta().getEnchants();
+                    Map<Enchantment, Integer> finalEnchantments = new HashMap<>();
+
+                    for (Enchantment enchantment : enchantmentSet(input)) {
+                        if (enchantment.getMaxLevel() > 1) {
+                            if (levelsCanBeAdded(enchantment, input, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, enchantmentStorageMeta.getStoredEnchantLevel(enchantment) + 1);
+                            } else if (firstInputIsHigher(enchantment, input, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, enchantmentStorageMeta.getStoredEnchantLevel(enchantment));
+                            } else if (enchNotMoreThanDouble(enchantment, input, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, enchantmentStorageMeta.getStoredEnchantLevel(enchantment));
+                            }
+                        }
+                    }
+
+                    for (Enchantment enchantment : enchantmentSet(input1)) {
+                        if (enchantment.getMaxLevel() > 1) {
+                            if (firstInputIsHigher(enchantment, input1, input, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, enchantmentStorageMeta1.getStoredEnchantLevel(enchantment));
+                            } else if (enchNotMoreThanDouble(enchantment, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, enchantmentStorageMeta1.getStoredEnchantLevel(enchantment));
+                            }
+                        }
+                    }
+                    // add enchants to result from map and save result
+                    event.setResult(createResult(input, input1, result, finalEnchantments));
+                } else if (!(input.getItemMeta() instanceof EnchantmentStorageMeta) && input1.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta1) {
+                    final Map<Enchantment, Integer> resultEnchantments = result.getItemMeta().getEnchants();
+                    Map<Enchantment, Integer> finalEnchantments = new HashMap<>();
+
+                    for (Enchantment enchantment : enchantmentSet(input)) {
+                        if (enchantment.getMaxLevel() > 1) {
+                            if (levelsCanBeAdded(enchantment, input, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, input.getEnchantmentLevel(enchantment) + 1);
+                            } else if (firstInputIsHigher(enchantment, input, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, input.getEnchantmentLevel(enchantment));
+                            } else if (enchNotMoreThanDouble(enchantment, input, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, input.getEnchantmentLevel(enchantment));
+                            }
+                        }
+                    }
+
+                    for (Enchantment enchantment : enchantmentSet(input1)) {
+                        if (enchantment.getMaxLevel() > 1) {
+                            if (firstInputIsHigher(enchantment, input1, input, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, enchantmentStorageMeta1.getStoredEnchantLevel(enchantment));
+                            } else if (enchNotMoreThanDouble(enchantment, input1, resultEnchantments)) {
+                                // add enchantment to map
+                                finalEnchantments.put(enchantment, enchantmentStorageMeta1.getStoredEnchantLevel(enchantment));
+                            }
+                        }
+                    }
+                    // add enchants to result from map and save result
+                    event.setResult(createResult(input, input1, result, finalEnchantments));
+                }
+            }
+        }
+    }
+
+    @NotNull
+    private static ItemStack createResult(ItemStack input, ItemStack input1, ItemStack result, Map<Enchantment, Integer> finalEnchantments) {
+        if (result.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta) {
+            finalEnchantments.forEach((enchantment, level) -> enchantmentStorageMeta.addStoredEnchant(enchantment, level, true));
+            result.setItemMeta(enchantmentStorageMeta);
+        } else {
+            Set<Enchantment> conflicting = getConflicting(enchantmentSet(input), enchantmentSet(input1));
+            if (!conflicting.isEmpty() && !result.getType().equals(Material.ENCHANTED_BOOK)) conflicting.forEach(finalEnchantments::remove);
+            result.addUnsafeEnchantments(finalEnchantments);
+        }
+        return result;
+    }
+
+    @NotNull
+    private static Set<Enchantment> enchantmentSet(ItemStack input) {
+        return input.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta ? enchantmentStorageMeta.getStoredEnchants().keySet() : input.getEnchantments().keySet();
+    }
+
+    @NotNull
+    private static Set<Enchantment> getConflicting(Set<Enchantment> inputEnchantments, Set<Enchantment> enchantmentsToCheck) {
+        if (inputEnchantments != null && enchantmentsToCheck != null) {
+            for (Enchantment enchantment : inputEnchantments) {
+                enchantmentsToCheck.removeIf(check -> check.conflictsWith(enchantment));
+            }
+        }
+        return enchantmentsToCheck;
+    }
+
+    private static boolean levelsCanBeAdded(final Enchantment enchantment, final ItemStack input, final ItemStack input1, final Map<Enchantment, Integer> resultEnchantments) {
+        if (input.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta && input1.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta1) {
+            return enchantmentStorageMeta.getStoredEnchantLevel(enchantment) == enchantmentStorageMeta1.getStoredEnchantLevel(enchantment)
+                    && (!resultEnchantments.containsKey(enchantment) || resultEnchantments.get(enchantment) < enchantmentStorageMeta.getStoredEnchantLevel(enchantment) + 1)
+                    && !(enchantmentStorageMeta.getStoredEnchantLevel(enchantment) + 1 > enchantment.getMaxLevel() * 2);
+        } else if (!(input.getItemMeta() instanceof EnchantmentStorageMeta) && input1.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta1) {
+            return input.getEnchantmentLevel(enchantment) == enchantmentStorageMeta1.getStoredEnchantLevel(enchantment)
+                    && (!resultEnchantments.containsKey(enchantment) || resultEnchantments.get(enchantment) < input.getEnchantmentLevel(enchantment) + 1)
+                    && !(input.getEnchantmentLevel(enchantment) + 1 > enchantment.getMaxLevel() * 2);
+        }
+        return input.getEnchantmentLevel(enchantment) == input1.getEnchantmentLevel(enchantment)
+                && (!resultEnchantments.containsKey(enchantment) || resultEnchantments.get(enchantment) < input.getEnchantmentLevel(enchantment) + 1)
+                && !(input.getEnchantmentLevel(enchantment) + 1 > enchantment.getMaxLevel() * 2);
+    }
+
+    private static boolean firstInputIsHigher(final Enchantment enchantment, final ItemStack input, final ItemStack input1, final Map<Enchantment, Integer> resultEnchantments) {
+        if (input.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta
+                && input1.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta1) {
+            return enchantmentStorageMeta.getStoredEnchantLevel(enchantment) > enchantmentStorageMeta1.getStoredEnchantLevel(enchantment)
+                    && (!resultEnchantments.containsKey(enchantment) || resultEnchantments.get(enchantment) < enchantmentStorageMeta.getStoredEnchantLevel(enchantment));
+        } else if (!(input.getItemMeta() instanceof EnchantmentStorageMeta)
+                && input1.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta1) {
+            return input.getEnchantmentLevel(enchantment) > enchantmentStorageMeta1.getStoredEnchantLevel(enchantment)
+                    && (!resultEnchantments.containsKey(enchantment) || resultEnchantments.get(enchantment) < input.getEnchantmentLevel(enchantment));
+        }
+        return input.getEnchantmentLevel(enchantment) > input1.getEnchantmentLevel(enchantment)
+                && (!resultEnchantments.containsKey(enchantment) || resultEnchantments.get(enchantment) < input.getEnchantmentLevel(enchantment));
+    }
+
+    private static boolean enchNotMoreThanDouble(final Enchantment enchantment, final ItemStack input, final Map<Enchantment, Integer> resultEnchantments) {
+        if (input.getItemMeta() instanceof final EnchantmentStorageMeta enchantmentStorageMeta) {
+            return !(enchantmentStorageMeta.getStoredEnchantLevel(enchantment) > enchantment.getMaxLevel() * 2)
+                    && (!resultEnchantments.containsKey(enchantment) || resultEnchantments.get(enchantment) < enchantmentStorageMeta.getStoredEnchantLevel(enchantment));
+        }
+        return !(input.getEnchantmentLevel(enchantment) > enchantment.getMaxLevel() * 2)
+                && (!resultEnchantments.containsKey(enchantment) || resultEnchantments.get(enchantment) < input.getEnchantmentLevel(enchantment));
+    }
+
+}
